416cea94e66f947ff4713fddd43b344f70463ff0
chrisalvares <mail@chrisalvares.com>
2013-11-26 22:04:28 -0600

    moved port back to 3000

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var httpProxy = require('http-proxy')
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 80);
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'app/views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
