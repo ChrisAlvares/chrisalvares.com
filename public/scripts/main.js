49a4b2eb85a34a764a61b42387941be03e764eed
chrisalvares <mail@chrisalvares.com>
2014-02-16 23:24:43 -0600

    Added high charts minified version

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -4,12 +4,12 @@ requirejs.config({
     paths: {
         component: '../components',
         async: '../components/requirejs-plugins/src/async',
-        jquery:'../components/jquery/jquery',
+        jquery:'../components/jquery/jquery.min',
         jqueryui:'../components/jquery-ui/ui/minified/jquery-ui.min', 
         bootstrap:'../components/bootstrap/dist/js/bootstrap.min',
         underscore:'../components/underscore/underscore-min',
         text:'../components/requirejs-text/text',
-        highcharts:'../components/highcharts.com/js/highcharts.src',
+        highcharts:'../components/highcharts.com/js/highcharts',
         d3:'../components/d3/d3.min',
         apicalls:'/apicalls?extension='
     },
